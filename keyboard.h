15e9f0bb7e19e34caefcb13097dede5345067345
Oleg Shitikov <shitikov.om@phystech.edu>
2020-11-18 20:23:40 +0300

    state get was added

--- keyboard.h
+++ keyboard.h
@@ -3,6 +3,17 @@
 #include <stdint.h>
 #include "idt.h"
 
+struct keyboard_state {
+    uint32_t shift_pressed;
+    uint32_t capslock_pressed;
+    uint32_t alt_pressed;
+    uint32_t ctrl_pressed;
+    uint32_t numlock_pressed;
+    uint32_t caps_on;
+    uint32_t num_on;
+};
 
+struct keyboard_state get_keyboard_state();
+//Keyboard support states of shift, ctl, alt, caps_lck and num_lck, uppercase letters on shift and caps lock
 void keyboard_interact();
 
